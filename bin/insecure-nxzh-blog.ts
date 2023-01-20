da3d08d386c82f4ac9153646766b4c272576698e
nxzh <zhangnaixiao@outlook.com>
2023-01-20 23:51:47 +0900

    Initial commit

---
+++ bin/insecure-nxzh-blog.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { InsecureNxzhBlogStack } from '../lib/insecure-nxzh-blog-stack';
+
+const app = new cdk.App();
+new InsecureNxzhBlogStack(app, 'InsecureNxzhBlogStack');
